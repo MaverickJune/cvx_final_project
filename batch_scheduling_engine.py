4c4a8597693291db91ad2b5ad8263b628ccf4f1b
Maverick_June <wjbang@snu.ac.kr>
2024-11-28 16:21:35 +0900

    completed and fully debugged

--- batch_scheduling_engine.py
+++ batch_scheduling_engine.py
@@ -10,7 +10,7 @@ from utils import format_example, count_tokens, prepare_dataset_for_speed_eval, 
 
 class BatchSchedulingEngine:
     def __init__(self, model_name="meta-llama/Llama-3.1-8B-Instruct", batch_size=4, seed=42, pad_token="<|pad|>", strategy="OPTIMAL", max_new_tokens=50, 
-                 requests: dict={}, slo_list: list=[], penalty="quadratic"):
+                 requests: dict={}, slo_list: list=[], penalty="quadratic", debug=False):
         seed_everything(seed)
         tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
         tokenizer.add_special_tokens({"pad_token": pad_token})
@@ -49,6 +49,7 @@ class BatchSchedulingEngine:
         self.decode_latency_pred = []
         self.process_time_pred = [] # prefill + decode, expected
         self.actual_process_time = [] # actual process time
+        self.actual_finish_time = [] # actual finish time
         self.run_profile()
         
         self.slo_list = slo_list # should be represented in seconds (not ms)
@@ -60,7 +61,22 @@ class BatchSchedulingEngine:
         
         self.running_order = self.generate_schedule()
         self.add_items_to_queue()
-            
+        
+        if debug:
+            self.init_debug()
+        
+    def init_debug(self):
+        print("Debug mode is on")
+        print("Prefill parameters: ", self.prefill_params)
+        print("Decode parameters: ", self.decode_params)
+        print("Prefill latency predictions: ", self.prefill_latency_pred)
+        print("Decode latency predictions: ", self.decode_latency_pred)
+        print("Process time predictions: ", self.process_time_pred)
+        print("Actual process time: ", self.actual_process_time)
+        print("SLO list: ", self.slo_list)
+        print("Running order: ", self.running_order)
+        print("Strategy: ", self.strategy)
+        
         
     def register_requests(self, questions: dict):
         datasets = [prepare_dataset_for_speed_eval(question, self.batch_size, self.tokenizer).to("cuda") for question in questions]
@@ -79,6 +95,9 @@ class BatchSchedulingEngine:
         
         self.prefill_latency_pred.extend(prefill_latencies_pred)
         self.decode_latency_pred.extend(decoding_latencies_pred)
+        
+        self.actual_process_time = [-1] * self.num_requests
+        self.actual_finish_time = [-1] * self.num_requests
     
         
     def generate_schedule(self) -> list[int]:
@@ -132,23 +151,35 @@ class BatchSchedulingEngine:
         
         
     def add_items_to_queue(self):
-        for data in self.datasets:
-            asyncio.get_event_loop().call_soon_threadsafe(self.data_queue.put_nowait, data)
+        for index in self.running_order:
+            data = self.datasets[index]
+            item = (index, data)
+            asyncio.get_event_loop().call_soon_threadsafe(self.data_queue.put_nowait, item)
             
             
     def analyze_and_record(self):
         log_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log")
-        log_file_path = os.path.join(log_dir_path, f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
+        log_file_path = os.path.join(log_dir_path, f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json")
         os.makedirs(log_dir_path, exist_ok=True)
         
-        slo_violation = self.profile_penalty(self.actual_process_time, self.slo_list)
+        temp_cumsum = 0
+        for idx in self.running_order:
+            self.actual_finish_time[idx] = temp_cumsum + self.actual_process_time[idx]
+            temp_cumsum = self.actual_finish_time[idx]
+            
+        slo_violation = self.profile_penalty(self.actual_finish_time, self.slo_list)
+        individual_slo_violation = [self.profile_penalty(self.actual_finish_time[index], self.slo_list[index]) for index in range(self.num_requests)]
         
         log_results = {
+            "predicted_prefill_latencies": self.prefill_latency_pred,
+            "predicted_decode_latencies": self.decode_latency_pred,
             "predicted_latencies": self.process_time_pred,
             "actual_latencies": self.actual_process_time,
+            "actual_finish_time": self.actual_finish_time,
             "slo_list": self.slo_list,
             "running_order": self.running_order,
-            "slo_violation": slo_violation,
+            "individual_slo_violation": individual_slo_violation,
+            "total_slo_violation": slo_violation,
             "strategy": self.strategy,
         }
         
@@ -156,15 +187,16 @@ class BatchSchedulingEngine:
             json.dump(log_results, f, indent=4)
         
         
-    async def llm_inference(self, data):
+    async def llm_inference(self, data, index, warmup=False):
         torch.cuda.synchronize()
         start = torch.cuda.Event(enable_timing=True)
-        end = torch.cuda.Evennt(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
         start.record()
         outputs = self.model.generate(**data, max_new_tokens = self.max_new_tokens)
         end.record()
         torch.cuda.synchronize()
-        self.actual_process_time.append(start.elapsed_time(end) / 1000) # convert to seconds
+        if not warmup:
+            self.actual_process_time[index] = start.elapsed_time(end) / 1000 # convert to seconds
         
     
     async def process_queue(self):
@@ -173,9 +205,10 @@ class BatchSchedulingEngine:
         # warmup
         print("Warming up...")
         warmup_data = self.datasets[0]
+        index = -1
         torch.cuda.synchronize()
         for _ in range(WARMUP_STEP):
-            await self.llm_inference(warmup_data)
+            await self.llm_inference(warmup_data, index=index, warmup=True)
         torch.cuda.synchronize()
         print("Warming up done \n")
         
@@ -183,14 +216,14 @@ class BatchSchedulingEngine:
         print("Processing queue...")
         while True:
             try:
-                data = await asyncio.wait_for(self.data_queue.get(), timeout=1)
-                await self.llm_inference(data)
+                index, data = await asyncio.wait_for(self.data_queue.get(), timeout=1)
+                await self.llm_inference(data, index)
                 self.data_queue.task_done()
                 
             except asyncio.TimeoutError:
                 if self.data_queue.empty():
                     self.processing = False
                     break
-        print("Processing done")
+        print("Processing done \n")
         
     
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import asyncio
+import argparse
 from batch_scheduling_engine import BatchSchedulingEngine
 
+def argparser():
+    parser = argparse.ArgumentParser(description="Batch scheduling engine")
+    parser.add_argument("--model_name", type=str, default="meta-llama/Llama-3.1-8B-Instruct", help="Model name")
+    parser.add_argument("--batch_size", type=int, default=4, help="Batch size")
+    parser.add_argument("--seed", type=int, default=42, help="Random seed")
+    parser.add_argument("--pad_token", type=str, default="<|pad|>", help="Pad token")
+    parser.add_argument("--strategy", type=str, default="OPTIMAL", choices=["OPTIMAL", "RANDOM", "SRTF", "FIFO"], help="Scheduling strategy")
+    parser.add_argument("--max_new_tokens", type=int, default=50, help="Maximum new tokens")
+    parser.add_argument("--penalty", type=str, default="quadratic", help="Penalty function")
+    parser.add_argument("--debug", action="store_true", help="Debug mode")
+    return parser
+
 async def main():
+    arparser = argparser()
+    args = arparser.parse_args()
+    
     # Define requests and SLOs
     requests = [
         {"question": "What’s your favorite color?"},
@@ -15,14 +31,44 @@ async def main():
         {"question": "Suppose you were tasked with developing a program to teach empathy and kindness in schools. What activities or lessons would you include, and how would you measure its success?"},
         {"question": "If you could collaborate with a global organization to tackle a major issue like climate change or poverty, what strategies would you propose, and how would you ensure long-term positive outcomes?"}
     ]
-    slo_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # should be modified to realistic values later !!
+    slo_list = [23, 18, 3, 13, 28, 5, 10, 12, 19, 20] 
     
     # Run the batch scheduling engine
-    engine = BatchSchedulingEngine(requests=requests, slo_list=slo_list)
+    engine = BatchSchedulingEngine(requests=requests, slo_list=slo_list, strategy=args.strategy, debug=False)
     await engine.process_queue()
     
     # profile and save the results
     engine.analyze_and_record()
+    print("Recorded the results")
 
 if __name__ == "__main__":
     asyncio.run(main())
+    
+# slo reference
+"""
+# total latency measured 
+2.88212266998291,
+2.8948785522460936,
+2.9123132751464844,
+2.951351406860352,
+2.9471540191650387,
+3.0605486785888676,
+3.0861006866455076,
+3.070599656677246,
+3.08922907409668,
+3.0943182128906246
+"""
+
+"""
+# shuffled, cumsum, and reordered
+26.902515545654296,
+20.894812847900393,
+3.08922907409668,
+15.087621020507815,
+29.988616232299805,
+6.159828730773926,
+9.041951400756837,
+11.993302807617189,
+17.98249957275391,
+23.84196686706543
+"""
